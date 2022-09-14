82f24b527cbf0d1a39762ae4dfc3be94d5e8f866
1tsak <hey.a2v10@gmail.com>
2022-09-14 20:02:20 +0530

    Added User routes model and controller files

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
-const colors = require ('colors');
+const colors = require('colors');
+require('dotenv').config();
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use('/api/users', require('./routes/userRoutes'));
+
+app.listen(port, () => {
+    console.log(`Server running on http://localhost:${port}`.yellow);
+
+})
